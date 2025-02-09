c4b83ba593b9b16331627fe2e41f64e2276d5f80
vishwajeet <awalevishwajeet@gmail.com>
2025-02-10 00:27:39 +0530

    added backspace btn

--- app.js
+++ app.js
@@ -1,6 +1,7 @@
 //calculator program
 
 const display = document.querySelector("input");
+const backspaceButton = document.querySelector("#backspaceButton");
 
 function appendToDisplay(input) {
   display.value += input;
@@ -12,4 +13,9 @@ function calculate() {
 
 function clearDisplay() {
   display.value = "";
+}
+
+function backspaceFunction() {
+  let currentValue = display.value;
+  display.value = currentValue.substring(0, currentValue.length - 1);
 }
